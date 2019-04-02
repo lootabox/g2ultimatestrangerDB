fb352690987f9893bd00835c8cfec1a81d530e7f
lootabox <tupelibox@gmail.com>
2019-04-03 02:04:58 +0300

    Beautified sources

--- hamster_eng.js
+++ hamster_eng.js
@@ -6,27 +6,22 @@ var __reg_exp_key_value = new RegExp('([^=]*)=', 'i');
 //if (top.location.href.substr(0,4) == 'http') top.location.href="http://mondgesaenge.de/G2DB";
 
 
-function _ParseString(s)
-{
+function _ParseString(s) {
 	arr = __reg_exp_key_value.exec(s);
-	if (arr != null)
-	{
+	if (arr != null) {
 		name = arr[1];
 		value = s.substr(name.length + 1);
 		PARAM[name] = unescape(value);
 	}
 	return '';
-	
+
 }
 
-function _ParseList(s)
-{
-	for (i = 0; i < s.length; ++i)
-	{
-		if (s.charAt(i) == '&')
-		{
+function _ParseList(s) {
+	for (i = 0; i < s.length; ++i) {
+		if (s.charAt(i) == '&') {
 			if (i > 0) _ParseString(s.substr(0, i));
-			s = s.substr(i+1);
+			s = s.substr(i + 1);
 			i = 0;
 		}
 	}
@@ -34,19 +29,17 @@ function _ParseList(s)
 	return;
 }
 
-_ParseList(location.search.substr(1));	
+_ParseList(location.search.substr(1));
 
 
 ////////////////////////////
 
-function INCLUDE(filename)
-{
+function INCLUDE(filename) {
 	document.write('<sc' + 'ript src="' + filename + '"></sc' + 'ript>');
 }
 
 
-function OUT(s)
-{
+function OUT(s) {
 	s = s.replace(/ä/g, "&auml;");
 	s = s.replace(/ö/g, "&ouml;");
 	s = s.replace(/ü/g, "&uuml;");
@@ -54,15 +47,15 @@ function OUT(s)
 	s = s.replace(/Ö/g, "&Ouml;");
 	s = s.replace(/Ü/g, "&Uuml;");
 	s = s.replace(/ß/g, "&szlig;");
-	
-/*
-	if (highlight = PARAM["highlight"])
-	{
-		highlight = highlight.replace(/\+/g, " ");
-		reg = new RegExp(highlight, "gi");
-		s = s.replace(reg, "<b>$&</b>");
-	}
-*/	
+
+	/*
+		if (highlight = PARAM["highlight"])
+		{
+			highlight = highlight.replace(/\+/g, " ");
+			reg = new RegExp(highlight, "gi");
+			s = s.replace(reg, "<b>$&</b>");
+		}
+	*/
 	document.write(s)
 }
 
--- navi_eng.js
+++ navi_eng.js
@@ -1,20 +1,16 @@
 var NAVI = new Array();
 
-NAVI[NAVI.length] = {name:'Ultimate Stranger', url:'menu_eng.htm?data=data_guide_eng.js&', data:'data_guide_eng.js'};
-//NAVI[NAVI.length] = {name:'Quests', url:'menu_eng.htm?data=data_quest_eng.js&', data:'data_quest_eng.js'};
-//NAVI[NAVI.length] = {name:'Persons', url:'menu_eng.htm?data=data_npc_eng.js&', data:'data_npc_eng.js'};
-//NAVI[NAVI.length] = {name:'Locations', url:'menu_eng.htm?data=data_orte_eng.js&', data:'data_orte_eng.js'};
-//NAVI[NAVI.length] = {name:'Maps', url:'menu_eng.htm?data=data_map_eng.js&', data:'data_map_eng.js'};
+NAVI[NAVI.length] = { name: 'Ultimate Stranger', url: 'menu_eng.htm?data=data_guide_eng.js&', data: 'data_guide_eng.js' };
+//NAVI[NAVI.length] = { name: 'Quests', url: 'menu_eng.htm?data=data_quest_eng.js&', data: 'data_quest_eng.js' };
+//NAVI[NAVI.length] = { name: 'Persons', url: 'menu_eng.htm?data=data_npc_eng.js&', data: 'data_npc_eng.js' };
+//NAVI[NAVI.length] = { name: 'Locations', url: 'menu_eng.htm?data=data_orte_eng.js&', data: 'data_orte_eng.js' };
+//NAVI[NAVI.length] = { name: 'Maps', url: 'menu_eng.htm?data=data_map_eng.js&', data: 'data_map_eng.js' };
 
 
-function GetNavIndex()
-{
-	if (PARAM.data)
-	{
-		for (i=0; i < NAVI.length; ++i)
-		{
-			if (PARAM.data == NAVI[i].data)
-			{
+function GetNavIndex() {
+	if (PARAM.data) {
+		for (i = 0; i < NAVI.length; ++i) {
+			if (PARAM.data == NAVI[i].data) {
 				return i;
 			}
 		};
