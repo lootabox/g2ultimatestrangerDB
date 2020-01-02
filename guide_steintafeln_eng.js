d08e6d0dddad94162cb792e9a28214ed8da4ac97
lootabox <tupelibox@gmail.com>
2020-01-02 20:00:29 +0200

    Added mod info page for stone tablets

---
+++ guide_steintafeln_eng.js
@@ -0,0 +1,175 @@
+
+DATA[DATA.length] = {section:"Old stone tablets"}
+DATA[DATA.length] = {id:"steintafel_1", pic:"screens/steintafeln/steintafel_alt.jpg", name:"Old stone tablet", value:"5", effect:"depending upon amount of various bonuses from Vatras (potions, scrolls or mana)",note:"\
+<nobr><li>from the <b>bandits</b> (at the bridge near <b>Akil's farm</b>) - quest from <b>Erol</b> (3 old stone tablets)</li></nobr>\
+<nobr><li><b>Chapel</b> near <b>Onar's farm</b></li></nobr>\
+<nobr><li><b>Chest</b> near <b>Skip</b></li></nobr>\
+<nobr><li><b>House of the scholars</b>, room on the right side (3 old stone tablets)</li></nobr>\
+<nobr><li><b>Jharkendar portal temple</b>, on the right side before the teleport</li></nobr>\
+<nobr><li><b>Khorinis portal temple</b></li></nobr>\
+<nobr><li><b>X treasure cross</b> in the ruins at the destroyed bridge</li></nobr>\
+<nobr><li><b>X treasure cross</b></li></nobr>\
+<nobr><li><b>X treasure cross</b></li></nobr>\
+<nobr><li><b>X treasure cross</b></li></nobr>\
+<nobr><li><b>near</b> the Stonehenge tomb</li></nobr>\
+<nobr><li><b>second tower</b> of Dexter</li></nobr>\
+<nobr><li>Chest in the <b>house of the guardians of the dead</b></li></nobr>\
+<nobr><li>Chest in the <b>ruins</b> on the right side before <b>Quarhodron's grave</b></li></nobr>\
+<nobr><li>Chest in the <b>ruins</b></li></nobr>\
+<nobr><li>Reward from <b>Morgan</b> when you bring back his ring which has <b>Hank</b> and <b>Angus</b>.</li></nobr>\
+<nobr><li>Use the ladders, <b>X treasure cross</b></li></nobr>\
+<nobr><li>at Xardas on the <b>table</b></li></nobr>\
+<nobr><li>at the <b>fire lizards</b> near <b>Xardas tower</b></li></nobr>\
+<nobr><li>from the <b>Hermit</b> as a reward when you give him clothes</li></nobr>\
+<nobr><li>in the <b>cave</b></li></nobr>\
+<nobr><li>near <b>Isgaroth</b></li></nobr>\
+<nobr><li>on the <b>top</b> of the pyramid</li></nobr>\
+<nobr><li>small <b>pyramid</b></li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of Strength"}
+DATA[DATA.length] = {id:"steintafel_20", pic:"screens/steintafeln/steintafel_staerke3.jpg", name:"Stone tablet of Strength I", value:"100", effect:"+1 to strength", note:"\
+<nobr><li>in the <b>cave</b> at the shadowbeast</li></nobr>\
+<nobr><li><b>beside</b> the <b>cave</b> of field raiders</li></nobr>\
+<nobr><li>chest in the <b>ruins</b> on the right side before <b>Quarhodron's grave</b></li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_21", pic:"screens/steintafeln/steintafel_staerke3.jpg", name:"Stone tablet of Strength II", value:"100", effect:"+2 to strength", note:"\
+<nobr><li>in the <b>forrest</b> behind <b>Onar's farm</b></li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_22", pic:"screens/steintafeln/steintafel_staerke3.jpg", name:"Stone tablet of Strength III", value:"100", effect:"+3 to strength", note:"\
+<nobr><li><b>House of the scholars</b>, in the middle</li></nobr>\
+<nobr><li><b>room</b> on the right side after the second trap</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of Dexterity"}
+DATA[DATA.length] = {id:"steintafel_8", pic:"screens/steintafeln/steintafel_geschick3.jpg", name:"Stone tablet of Dexterity I", value:"100", effect:"+1 to dexterity", note:"\
+<nobr><li>near the <b>lurkers</b></li></nobr>\
+<nobr><li>on the <b>hill</b> - go right from the <b>orc ram</b></li></nobr>\
+<nobr><li><b>X treasure cross</b></li></nobr>\
+<nobr><li><b>Chest</b> in Quarhodron's grave</li></nobr>\
+<nobr><li>in the inventory of the dead <b>bandit</b> at Rhademes</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_9", pic:"screens/steintafeln/steintafel_geschick3.jpg", name:"Stone tablet of Dexterity II", value:"100", effect:"+2 to dexterity", note:"\
+<nobr><li><b>behind</b>  the waterfall at Buster's hut</li></nobr>\
+<nobr><li>in the <b>hoard</b> of the <b>rock dragon</b></li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_10", pic:"screens/steintafeln/steintafel_geschick3.jpg", name:"Stone tablet of Dexterity III", value:"100", effect:"+3 to dexterity", note:"\
+<nobr><li>from the <b>Hermit</b> as a reward when you give him clothes</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of Magic"}
+DATA[DATA.length] = {id:"steintafel_14", pic:"screens/steintafeln/steintafel_magie3.jpg", name:"Stone tablet of Magic I", value:"100", effect:"+1 to mana", note:"\
+<nobr><li><b>third beach</b></li></nobr>\
+<nobr><li><b>Bandits camp</b></li></nobr>\
+<nobr><li><b>beside</b> the statue of Innos</li></nobr>\
+<nobr><li><b>cave</b> with teleporter (get the key from <b>Orlan</b>)</li></nobr>\
+<nobr><li>on the <b>top</b> of the pyramid</li></nobr>\
+<nobr><li>in the mine, <b>X treasure cross</b></li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_15", pic:"screens/steintafeln/steintafel_magie3.jpg", name:"Stone tablet of Magic II", value:"100", effect:"+2 to mana", note:"\
+<nobr><li><b>crypt</b> near <b>Onar' farm</b></li></nobr>\
+<nobr><li><b>first beach</b></li></nobr>\
+<nobr><li>in the <b>hoard</b> of the <b>ice dragon</b></li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_16", pic:"screens/steintafeln/steintafel_magie3.jpg", name:"Stone tablet of Magic III", value:"100", effect:"+3 to mana", note:"\
+<nobr><li>at the <b>snappers</b>, in front of the <b>black troll</b> on the right side</li></nobr>\
+<nobr><li><b>crypt</b> near <b>Onar' farm</b></li></nobr>\
+<nobr><li>Chest in the <b>ruins</b> at the destroyed bridge</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of Life Energy"}
+DATA[DATA.length] = {id:"steintafel_11", pic:"screens/steintafeln/steintafel_leben3.jpg", name:"Stone tablet of Life Energy I", value:"100", effect:"+2 to life energy", note:"\
+<nobr><li>behind the <b>stone circle</b> near <b>Lobart's farm</b></li></nobr>\
+<nobr><li><b>Khorinis portal temple</b>, in the inventory of the dead stone sentinel</li></nobr>\
+<nobr><li>on the <b>top</b> of the pyramid</li></nobr>\
+<nobr><li><b>cave</b> at the <b>pyramid</b></li></nobr>\
+<nobr><li><b>Stone circle</b></li></nobr>\
+<nobr><li>near the <b>circle of the sun</b></li></nobr>\
+<nobr><li><b>Khorinis portal temple</b>, in the right corner at Saturas</li></nobr>\
+<nobr><li><b>cave</b> near the field raiders</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_12", pic:"screens/steintafeln/steintafel_leben3.jpg", name:"Stone tablet of Life Energy II", value:"100", effect:"+4 to life energy", note:"\
+<nobr><li>at the <b>orcs</b></li></nobr>\
+<nobr><li>at the paladins, on the <b>box</b></li></nobr>\
+<nobr><li>left <b>bank</b></li></nobr>\
+<nobr><li>near the <b>ghost light</b></li></nobr>\
+<nobr><li>left <b>room</b> after the light trap (or second trap) of the adanos temple</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_13", pic:"screens/steintafeln/steintafel_leben3.jpg", name:"Stone tablet of Life Energy III", value:"100", effect:"+6 to life energy", note:"\
+<nobr><li><b>Jharkendar portal temple</b>, before the entrance/exit</li></nobr>\
+<nobr><li><b>House of the scholars</b>, in the middle of the house</li></nobr>\
+<nobr><li>Chest in the <b>cave</b> near the hut of the Hermit</li></nobr>\
+<nobr><li><b>Cave of the shadowbeast</b></li></nobr>\
+<nobr><li><b>cave</b> *The test of Fire*</li></nobr>\
+<nobr><li>Dexter's <b>mine</b></li></nobr>\
+<nobr><li><b>Dexter's camp</b>, after the bridge on the left side</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of One-Handed Combat"}
+DATA[DATA.length] = {id:"steintafel_17", pic:"screens/steintafeln/steintafel_1h3.jpg", name:"Stone tablet of One-Handed Combat I", value:"100", effect:"+1% to one-handed talent", note:"\
+<nobr><li>beside the <b>field raiders</b></li></nobr>\
+<nobr><li>on the <b>right</b> side behind the suspension bridge</li></nobr>\
+<nobr><li>in the <b>cave </b>on the way to the <b>stonehenge crypt</b></li></nobr>\
+<nobr><li>in a chest in the <b>cave</b> on the way to the <b>house of the guardians of the dead</b></li></nobr>\
+<nobr><li>in the inventory of the dead <b>bandit</b> near Rhademes</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_18", pic:"screens/steintafeln/steintafel_1h3.jpg", name:"Stone tablet of One-Handed Combat II", value:"100", effect:"+2% to one-handed talent", note:"\
+<nobr><li>at the rocks above the cave, at the <b>stairs</b></li></nobr>\
+<nobr><li><b>treasure island</b>, on the <b>right</b> side before the entrance, besides the meadow knotweed</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_19", pic:"screens/steintafeln/steintafel_1h3.jpg", name:"Stone tablet of One-Handed Combat III", value:"100", effect:"+3% to one-handed talent", note:"\
+<nobr><li><b>room</b> on the left side after the second trap</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of Two-Handed Combat"}
+DATA[DATA.length] = {id:"steintafel_23", pic:"screens/steintafeln/steintafel_2h3.jpg", name:"Stone tablet of Two-Handed Combat I", value:"100", effect:"+1% to two-handed talent", note:"\
+<nobr><li><b>beside</b> the statue of Innos</li></nobr>\
+<nobr><li>in a chest in the <b>ruins</b> at the destroyed brdige</li></nobr>\
+<nobr><li><b>cave</b> with minecrawlers - X cross</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_24", pic:"screens/steintafeln/steintafel_2h3.jpg", name:"Stone tablet of Two-Handed Combat II", value:"100", effect:"+2% to two-handed talent", note:"\
+<nobr><li>in a chest in the <b>house of the guardians of the dead</b></li></nobr>\
+<nobr><li>in the <b>cave</b> at the <b>black troll</b>, in the right corner</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_25", pic:"screens/steintafeln/steintafel_2h3.jpg", name:"Stone tablet of Two-Handed Combat III", value:"100", effect:"+3% to two-handed talent", note:"\
+<nobr><li>in <b>Malcom's</b> inventory</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of Archery"}
+DATA[DATA.length] = {id:"steintafel_2", pic:"screens/steintafeln/steintafel_bogen3.jpg", name:"Stone tablet of Archery I", value:"100", effect:"+1% to bow talent", note:"\
+<nobr><li>at the <b>Scavengers</b> on the way to the <b>monastery</b></li></nobr>\
+<nobr><li>in the <b>cave</b> at the <b>pyramid</b></li></nobr>\
+<nobr><li>in the <b>hoard</b> of the <b>rock dragon</b></li></nobr>\
+<nobr><li><b>near</b> the fire lizards</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_3", pic:"screens/steintafeln/steintafel_bogen3.jpg", name:"Stone tablet of Archery II", value:"100", effect:"+2% to bow talent", note:"\
+<nobr><li><b>Bandits cave</b> near the <b>ice area</b></li></nobr>\
+<nobr><li><b>behind</b> the <b>2 trolls</b></li></nobr>\
+<nobr><li><b>Treasure island</b>, in the <b>cave</b></li></nobr>\
+<nobr><li><b>direction</b> to the bandits cave</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_4", pic:"screens/steintafeln/steintafel_bogen3.jpg", name:"Stone tablet of Archery III", value:"100", effect:"+3% to bow talent", note:"\
+<nobr><li><b>House of the scholars</b>, <b>room</b> on the left side</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Stone tablets of Crossbow"}
+DATA[DATA.length] = {id:"steintafel_5", pic:"screens/steintafeln/steintafel_armbrust3.jpg", name:"Stone tablet of Crossbow I", value:"100", effect:"+1% to crossbow talent", note:"\
+<nobr><li><b>entrance</b> of the portal temple, on the left side</li></nobr>\
+<nobr><li>in the <b>cave</b> at the lake</li></nobr>\
+<nobr><li>near the <b>orcs</b> at the small bridge</li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_6", pic:"screens/steintafeln/steintafel_armbrust3.jpg", name:"Stone tablet of Crossbow II", value:"100", effect:"+2% to crossbow talent", note:"\
+<nobr><li><b>Lakeside</b></li></nobr>\
+<nobr><li><b>Cave of goblins</b></li></nobr>\
+"};
+DATA[DATA.length] = {id:"steintafel_7", pic:"screens/steintafeln/steintafel_armbrust3.jpg", name:"Stone tablet of Crossbow III", value:"100", effect:"+3% to crossbow talent", note:"\
+<nobr><li><b>House of the scholars</b>, <b>room</b> on the right side</li></nobr>\
+"};
+
+DATA[DATA.length] = {section:"Other stone tablets (Relics)"}
+DATA[DATA.length] = {id:"steintafel_26", pic:"screens/steintafeln/steintafel_rot.jpg", name:"Red stone tablet", value:"", effect:"Solution for the riddle in the house of the warriors", note:"<li>from <b>Esteban</b> (quest <b>'Attempted murder'</b> and <b>'Relics'</b>)</li>"}
+DATA[DATA.length] = {id:"steintafel_27", pic:"screens/steintafeln/steintafel_gelb.jpg", name:"Yellow stone tablet", value:"", effect:"Solution for the riddle in the house of the warriors", note:"<li>beside a chest in the <b>house of the scholars</b> (quest <b>'Relics'</b>)</li>"}
+DATA[DATA.length] = {id:"steintafel_28", pic:"screens/steintafeln/steintafel_gruen.jpg", name:"Green stone tablet", value:"", effect:"Solution for the riddle in the house of the warriors", note:"<li>in a chest in the <b>house of the healers</b> (Schlüssel bei einem der Steinwächter im Inventar) (quest <b>'Relics'</b>); must have it in your inventory in the temple of Adanos to talk with <b>Rhademes</b> about the last trap (quest <b>'The chambers of the temple'</b>)</li>"}
+DATA[DATA.length] = {id:"steintafel_29", pic:"screens/steintafeln/steintafel_blau.jpg", name:"Blue stone tablet", value:"", effect:"Solution for the riddle in the house of the warriors", note:"<li>in a chest in the <b>house of the priests</b> (quest <b>'Relics'</b>)</li>"}
+DATA[DATA.length] = {id:"steintafel_30", pic:"screens/steintafeln/steintafel_violett.jpg", name:"Purple stone tablet", value:"", effect:"Solution for the riddle in the house of the warriors", note:"<li>in a chest in the <b>house of the guardians of the dead</b> (quest <b>'Relics'</b>)</li>"}
+DATA[DATA.length] = {id:"steintafel_31", pic:"screens/steintafeln/steintafel_quarhodron.jpg", name:"Quarhodron's stone tablet", value:"", effect:"opens the gate to the temple of Adanos", note:"<li>from <b>Quarhodron</b> when you have answered his questions (quest <b>'The power of the guardians of the dead'</b>)</li>"}
